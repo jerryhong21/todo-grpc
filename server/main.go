0fd6640c0812b49c507d295ec36079985cbab9eb
Jerry Hong <jh1012037421@gmail.com>
2024-12-06 19:19:44 +1100

    implemented GetTodo function in server, refactored response handling from SC api

--- server/main.go
+++ server/main.go
@@ -53,6 +53,13 @@ type BulkDeleteTodoPayload struct {
 	TaskIDs []string `json:"ids"`
 }
 
+// // Standard struct for SafetyCulture API error response
+// type ScErrorResponse struct {
+// 	Code int `json:"code"`
+// 	Message string `json:"message"`
+// 	Details []any `json:"details"`
+// }
+
 // CreateTodo
 // Returns a pb.Todo object
 // context.Context is a type interaface (inherently a pointer) and therefore does not need a pointer
@@ -126,7 +133,7 @@ func (s *server) BulkDeleteTodo(ctx context.Context, req *pb.BulkDeleteTodoReque
 
 	// concatenates delete action prefix and actionId
 	ids := req.GetIds()
-	SC_DELETE_ACTION_URL := "https://api.safetyculture.io/tasks/v1/actions/delete"
+	SC_DELETE_TODO_URL := "https://api.safetyculture.io/tasks/v1/actions/delete"
 
 	// Create a payload struct, JSONIFY it
 	payloadData := BulkDeleteTodoPayload{
@@ -142,26 +149,21 @@ func (s *server) BulkDeleteTodo(ctx context.Context, req *pb.BulkDeleteTodoReque
 	// The below works for single deletion payload, the above works for bulk deletion
 	// payload := strings.NewReader(fmt.Sprintf(`{"ids":["%s"]}`, id))
 
-	deleteReq, _ := http.NewRequest("POST", SC_DELETE_ACTION_URL, payload)
+	deleteReq, _ := http.NewRequest("POST", SC_DELETE_TODO_URL, payload)
 
 	deleteReq.Header.Add("accept", "application/json")
 	deleteReq.Header.Add("content-type", "application/json")
 	deleteReq.Header.Add("authorization", "Bearer "+os.Getenv("SC_API_KEY"))
 
-	res, err := http.DefaultClient.Do(deleteReq)
-	if err != nil {
-		fmt.Printf(": error requesting SC API: %v", err)
-		return nil, fmt.Errorf("failed to send request to SafetyCulture API: %w", err)
+	res, _ := http.DefaultClient.Do(deleteReq)
+
+	body, err := handleResponse(res)
+	if body == nil && err != nil {
+		fmt.Println("The API returned with an error: %w", err)
+		return nil, err
 	}
 
-	defer res.Body.Close()
-	// there is no use for the body
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		fmt.Printf("Failed to read response body: %v", err)
-		return nil, fmt.Errorf("failed to process response from SafetyCulture API")
-	}
-
+	// if body is not nil, but empty - this signifies correct
 	if len(body) == 0 {
 		// remove the todo from our body
 		for _, id := range ids {
@@ -176,6 +178,68 @@ func (s *server) BulkDeleteTodo(ctx context.Context, req *pb.BulkDeleteTodoReque
 	// If so, then we need to update the behaviour of our function such that the valid IDs are removed
 
 	return nil, fmt.Errorf("error: Response from SC API:\n %v", string(body))
+}
+
+func (s *server) GetTodo(ctx context.Context, req *pb.GetTodoRequest) (*pb.Todo, error) {
+	id := req.GetId();
+	SC_GET_TODO_URL := "https://api.safetyculture.io/tasks/v1/actions/" + id
+
+
+	getReq, _ := http.NewRequest("GET", SC_GET_TODO_URL, nil)
+	getReq.Header.Add("accept", "application/json")
+
+	res, _ := http.DefaultClient.Do(getReq)
+	resBody, err := handleResponse(res)
+	if resBody == nil && err != nil {
+		fmt.Println("The API returned with an error: %w", err)
+		return nil, err
+	}
+
+	// else, return the response body
+	return s.todos[id], nil
+}
+
+
+// function to handle response
+// only checks for whether there are any errors
+func handleResponse(res *http.Response) ([]byte, error) {
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Printf("Failed to read response body: %v", err)
+		return nil, fmt.Errorf("failed to process response from SafetyCulture API")
+	}
+
+	// decode the response into a map of keys of type string, which maps to values of ANY kind
+	var result map[string]any
+	err = json.NewDecoder(res.Body).Decode(&result); 
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	// if the field "code" exists, then error
+	_, exists := result["code"]
+	if !exists {
+		fmt.Println("Normal response")
+		return body, nil
+	}
+	
+	// Else, print the error
+	return nil, fmt.Errorf("%v", body)
+	/*
+	
+	IF WE WANT OT HANDLE THE ERROR REPSONSE:
+
+	errRes := ScErrorResponse{}
+
+	// decode the json string contained in the "mesage field of the results map"
+	// breakdown:
+	// result["message"] is of type "any" - which means we need to make a type assertion: .(string)
+	// []byte(...) converts the string value into a byte slice since json.Unmarshal requires a byte slice
+	// finally, json.Unmarshal decodes JSON data, into a go strct
+	err = json.Unmarshal([]byte(result["message"].(string)), &errRes)
+	*/
 }
 
 // Main server
